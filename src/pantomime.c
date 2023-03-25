584db2b1af7c2f56c4f07ced17131166502c0e1a
Julianne Adams <julianne@julianneadams.info>
2023-03-25 07:29:55 -0400

    Add: host and port arguments

--- src/pantomime.c
+++ src/pantomime.c
@@ -17,6 +17,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
 
+#include <argp.h>
 #include <locale.h>
 #include <ncurses.h>
 #include <stdio.h>
@@ -24,8 +25,55 @@
 
 #include "mpdwrapper.h"
 
-int main()
+struct arguments {
+    char *args[1];
+    char *host;
+    int port;
+};
+
+static error_t parse_opt(int key, char *arg, struct argp_state *state)
 {
+    struct arguments *arguments = state->input;
+    switch (key) {
+        case 'h':
+            arguments->host = arg;
+            break;
+        case 'p':
+            arguments->port = atoi(arg);
+            break;
+        case ARGP_KEY_ARG:
+            /* Too many arguments. */
+            if (state->arg_num > 2) {
+                argp_usage(state);
+            }
+            arguments->args[state->arg_num] = arg;
+            break;
+        default:
+            return ARGP_ERR_UNKNOWN;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    const char *argp_program_version = "Pantomime 0.0.1";
+    static char argp_program_bug_address[] = "<julianne@julianneadams.info>";
+    static char doc[] = "An MPD client built on NCURSES.";
+    static char args_doc[] = "";
+    static struct argp_option options[] = {
+        {"host", 'h', "HOST", OPTION_ARG_OPTIONAL,
+         "The IP address or UNIX socket path of the MPD host."},
+        {"port", 'p', "PORT", OPTION_ARG_OPTIONAL,
+         "The port of the MPD host. Only used when connecting via IP address."},
+    };
+    static struct argp argp = {options, parse_opt, args_doc, doc};
+
+    struct arguments arguments;
+    arguments.host = "localhost";
+    arguments.port = 6600;
+
+    argp_parse(&argp, argc, argv, 0, 0, &arguments);
+
     /*
       setlocale(LC_ALL, "");
       initscr();
@@ -43,7 +91,10 @@ int main()
       endwin();
     */
 
-    struct mpdwrapper *mpd = mpdwrapper_new("127.0.0.1", 6600, 0);
+    printf("%s\n", argp_program_version);
+    printf("Please send bug reports to %s\n", argp_program_bug_address);
+
+    struct mpdwrapper *mpd = mpdwrapper_new(arguments.host, arguments.port, 0);
     if (!mpd) {
         fprintf(stderr, "Error connecting to MPD.\n");
         exit(EXIT_FAILURE);
